832029cda32b3ae2c3310e5e76f9cbc039c4b27c
Joao Chaves <joao.chaves@kodly.io>
2022-01-09 23:55:44 +0000

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import image from './logo.png';
 import './App.css';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
   return (
     <div className="App">
       <img src={image} alt="logo" className="logo" />
+      <Button onClick={() => navigate("/dashboard")}>
+        Entrar
+      </Button>
     </div >
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from './theme';
@@ -12,12 +12,12 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
